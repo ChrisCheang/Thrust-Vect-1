79f423b9272dc672de93ecae84f04c721375125c
ChrisCheang <128516153+ChrisCheang@users.noreply.github.com>
2023-11-04 18:11:23 +0000

    Update Actuator.py

--- Actuator.py
+++ Actuator.py
@@ -7,13 +7,13 @@ import matplotlib.pyplot as plot
 class Actuator:
     #  additional resource to consider: https://forum.ansys.com/uploads/attachments/3e825b35-df61-45f5-9693-a81101684b62/6f8fd9d8-ea1d-4478-9a91-aa81014cd231_nut-factor.pdf
     def __init__(self, force, vmax, scd=30, ratio=3, b=30*np.pi/180, t=0.8, d=4.48, mu=0.24):
-        self.w = force  # actuator force
-        self.vmax = vmax  # maximum actuator speed
-        self.scd = scd  # distance between motor shaft and actuator shaft
+        self.w = force  # actuator force in N
+        self.vmax = vmax  # maximum actuator speed in mm/s
+        self.scd = scd  # distance between motor shaft and actuator shaft in mm
         self.ratio = ratio # ratio between motor and actuator shaft
-        self.b = b  # metric thread angle
-        self.t = t  # thread pitch (currently t = 0.8 for M5)
-        self.d = d  # pitch centre diameter of thread (currently d = 4.48 for M5)
+        self.b = b  # metric thread angle in rad
+        self.t = t  # thread pitch (currently t = 0.8 for M5) in mm
+        self.d = d  # pitch centre diameter of thread (currently d = 4.48 for M5) in mm
         self.mu = mu  # thread coefficient of friction (conservative value)
 
     def rod_torque(self):
