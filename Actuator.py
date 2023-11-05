5a6e98350e1f242d1ccf6d480773fd2e0eaab695
ChrisCheang <128516153+ChrisCheang@users.noreply.github.com>
2023-11-05 00:41:52 +0000

    Update Actuator.py

--- Actuator.py
+++ Actuator.py
@@ -12,15 +12,22 @@ class Actuator:
         self.scd = scd  # distance between motor shaft and actuator shaft in mm
         self.ratio = ratio # ratio between motor and actuator shaft
         self.b = b  # metric thread angle in rad
-        self.t = t  # thread pitch (currently t = 0.8 for M5) in mm
+        self.t = t  # thread pitch (currently t = 0.8 for M5) in mm OR screw lead
         self.d = d  # pitch centre diameter of thread (currently d = 4.48 for M5) in mm
         self.mu = mu  # thread coefficient of friction (conservative value)
 
-    def rod_torque(self):
+    def rod_torque_nut(self):
         # using equation D.1.1 in mecheng data formula book with V-thread modification
+        # this is for lead screws; for ball screws use the one below
         tanphi = self.t/(np.pi*self.d)
         p = self.w * (tanphi + self.mu * (1 / math.cos(self.b))) / (1 - self.mu * (1 / math.cos(self.b)) * tanphi)
         return p * self.d * 10**(-3) / 2
+
+    def rod_torque(self):
+        # https://my.mech.utah.edu/~me7960/lectures/Topic4-BallscrewCalculations.pdf
+        # efficiency is estimated at 0.9 (https://monroeengineering.com/blog/pros-and-cons-of-using-ball-screws/#:~:text=Ball%20screws%20offer%20a%20high,just%2020%25%20to%2025%25.)
+        # screw lead = pitch when the thread is single start i.e. 1 pitch per rotation (https://www.thomsonlinear.com/en/support/tips/difference-between-screw-pitch-and-lead)
+        return self.w * self.t * 10**(-3) / (2 * np.pi * 0.9)
 
     def motor_torque(self):
         return self.rod_torque() / self.ratio
@@ -31,10 +38,13 @@ class Actuator:
     def motor_rpm(self):
         return self.rod_rpm() * self.ratio
 
+    def motor_power(self):
+        return self.motor_torque() * (self.motor_rpm()*2*np.pi/60)
+
 
 act1 = Actuator(force=600, vmax=30)
 
-print(act1.motor_torque())
+print(act1.rod_rpm())
 
 
 
